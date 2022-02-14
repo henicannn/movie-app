ea168e1995a8e36506d08f547664e1ec00f2038c
henicannn <hmanavadaria@gmail.com>
2022-02-14 18:23:06 -0500

    created react movie app

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Main from "./Components/Main";
+import Card from "./Components/Card";
+import "./Components/style.css";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <Main />
+    </>
+  );
+}
+
+export default App;
---
+++ src/Components/Card.jsx
@@ -0,0 +1,25 @@
+import react from "react";
+
+const Card = (movie) => {
+  console.log(movie.info);
+  let img_path = "https://image.tmdb.org/t/p/w300";
+  return (
+    <>
+      <div className="movie">
+        <img src={img_path + movie.info.poster_path}></img>
+        <div className="movie-details">
+          <div className="box">
+            <h4 className="title">{movie.info.title}</h4>
+            <p className="rating">{movie.info.vote_average}</p>
+          </div>
+          <div className="overview">
+            <h1>Overview</h1>
+            {movie.info.overview}
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Card;
